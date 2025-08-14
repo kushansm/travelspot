dda2a92b5fb2a124c43b624c7a64c9d25b3b974d
kushansm <kushandileep12@mail.com>
2025-08-14 12:19:32 +0530

    change image path

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,12 +6,13 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/effect-fade";
 
-const images: string[] = [
-  "src/assets/ninearch.jpg",
-  "src/assets/kandy.jpg",
-  "src/assets/beach.jpg",
-  "src/assets/sigiriya.jpg",
-];
+// Import images directly
+import NineArch from "../assets/ninearch.jpg";
+import Kandy from "../assets/kandy.jpg";
+import Beach from "../assets/beach.jpg";
+import Sigiriya from "../assets/sigiriya.jpg";
+
+const images: string[] = [NineArch, Kandy, Beach, Sigiriya];
 
 export const Hero = () => {
   return (
